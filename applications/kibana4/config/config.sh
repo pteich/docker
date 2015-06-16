4ed6557d63980eea028d29daafadc425c3bc1980
Peter Teich <peter.teich@motor.de>
2015-06-16 21:53:07 +0200

    Added Kibana4 image

---
+++ applications/kibana4/config/config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export ELASTICSEARCH=${ELASTICSEARCH:-http://127.0.0.1:9200}
+export KIBANA_INDEX=${KIBANA_INDEX:-.kibana}
+export DEFAULT_APP_ID=${DEFAULT_APP_ID:-discover}
+export REQUEST_TIMEOUT=${REQUEST_TIMEOUT:-300000}
+export SHARD_TIMEOUT=${SHARD_TIMEOUT:-0}
+export VERIFY_SSL=${VERIFY_SSL:-true}
+
+REPLACE=(
+ "s|^elasticsearch_url:.*$|elasticsearch_url: \"$ELASTICSEARCH\"|;"
+ "s|^kibana_index:.*$|kibana_index: \"$KIBANA_INDEX\"|;"
+ "s|^default_app_id:.*$|default_app_id: \"$DEFAULT_APP_ID\"|;"
+ "s|^request_timeout:.*$|request_timeout: $REQUEST_TIMEOUT|;"
+ "s|^shard_timeout:.*$|shard_timeout: $SHARD_TIMEOUT|;"
+ "s|^verify_ssl:.*$|verify_ssl: $VERIFY_SSL|;"
+)
+
+sed -i.bak -e "${REPLACE[*]}" /opt/kibana/config/kibana.yml
